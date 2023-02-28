ececc43383907a1a715a3e4c862559db0919f049
JayantiA <126594479+JayantiA@users.noreply.github.com>
2023-02-28 14:28:38 -0500

    Add files via upload

---
+++ untitled/src/main/java/com/example/fun/Game.java
@@ -0,0 +1,10 @@
+package com.example.fun;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter @Builder
+public class Game {
+    private Integer id;
+    private String text;
+}
---
+++ untitled/src/main/java/com/example/fun/ReadData.java
@@ -0,0 +1,26 @@
+package com.example.fun;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadData {
+
+   @DataProvider(name="sendData")
+    public static Object[] getData() throws IOException {
+       JsonPath js=new JsonPath(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"\\src\\test\\resources\\testData.json"))));
+
+       int nameCount=js.getInt("name.size()");
+
+       Object[] o=new Object[nameCount];
+       for(int index=0;index<nameCount;index++){
+           String name=js.get("name["+index+"]");
+           o[index]=name;
+       }
+
+       return o;
+   }
+}
