3a15d5b05ae2298664b25f6be176c8bb84a5bfa3
sai <binnu@KOWSHIK>
2023-03-05 13:22:36 +0530

    simple-game service:Sai

--- src/main/java/fun/Game.java
+++ src/main/java/fun/Game.java
@@ -3,8 +3,20 @@ package fun;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter @Builder
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
-    private Integer id;
-    private String text;
+private Integer id;
+private String text;
 }
