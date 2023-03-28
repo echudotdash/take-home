503829c5c0fe64971df591a38fbd128255559364
avinashpal1997 <1997avinashpal@gmail.com>
2023-03-28 03:22:11 -0400

    developed new branch

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -5,4 +5,14 @@ public class GameTests {
 int a= 3; 
 int b =6;
 int c = 4;
+        public void name()
+        {
+        	if(a<b)
+        	{
+        		System.out.println("a is less than b");
+        	}
+        }
+
+
 }
+
